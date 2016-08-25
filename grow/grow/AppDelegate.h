91a30be48fe7588b60c14783b260f9fcccc8fd8f
snow <925137520@qq.com>
2016-08-26 00:36:56 +0800

    初始化项目信息

---
+++ grow/grow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  grow
+//
+//  Created by Luca  on 16/8/26.
+//  Copyright © 2016年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
